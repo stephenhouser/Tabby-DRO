abc82e86e37ef58be0884bc5d5d9fade6407786c
Stephen Houser <stephenhouser@gmail.com>
2018-06-30 14:22:20 -0400

    intermediate about to rewrite grid

--- www/js/index.js
+++ www/js/index.js
@@ -4,14 +4,54 @@
     Modified 9 May 2013
     by Tom Igoe
 */
+class Axis {
+    var mode = 'abs';
+    var 
+    constructor() {
+
+    }
+};
+
+var axis_defaults = {
+    'mode': 'abs',  // abs, inc
+    'units': 'in',   // (in, mm), (rpm, sfm), (deg, rad)
+    'raw': 0.0,     // current reading (raw value)
+    'zero': 0.0,    // raw value that is zero position
+    'value': 0.0,   // current reading (relative to zero)
+};
+
 var app = {
-    macAddress: "20:17:12:05:01:59",  // get your mac address from bluetoothSerial.list
     chars: "",
+    macAddress: "20:17:12:05:01:59",
+
+    auto_connect: "20:17:12:05:01:59",     // or null if no auto-connect
+
+    touch_probe: false,
+
+    axis = {
+        'x': Object.assign({}, axis_defaults),
+        'y': Object.assign({}, axis_defaults),
+        'z': Object.assign({}, axis_defaults),
+        'w': Object.assign({}, axis_defaults),
+        't': Object.assign({}, axis_defaults),
+    },
+
+    points = [
+        [0.0, 0.0, 0.0, 0.0],
+        [1.0, 1.0, 0.0, 0.0],
+        [2.0, 2.0, 2.0, 0.0]
+    ],
+
+    tools = {
+        '1/8" mill': { 'name': '1/8" 4-flute mill', 'diameter': 0.125, 'units': 'in' },
+        '1/4" mill' : { 'name': '1/4" 4-flute mill', 'diameter': 0.25, 'units': 'in'},
+        '1/2" mill': { 'name': '1/2" 4-flute mill', 'diameter': 0.5, 'units': 'in' },
+    },
 
     /* Application constructor */
     initialize: function () {
         this.bindEvents();
-        console.log("Starting SimpleSerial app");
+        console.log("Starting Tabby DRO");
     },
 
     /* bind any events that are required on startup to listeners */
@@ -29,17 +69,18 @@ var app = {
             // list the available BT ports:
             bluetoothSerial.list(
                 function (results) {
-                    app.display(JSON.stringify(results));
+                    app.notify(JSON.stringify(results));
                 },
                 function (error) {
-                    app.display(JSON.stringify(error));
+                    app.notify(JSON.stringify(error));
                 }
             );
         }
 
         // if isEnabled returns failure, this function is called:
         var notEnabled = function () {
-            app.display("Bluetooth is not enabled.")
+            app.clear();
+            app.notify("Bluetooth is not enabled.")
         }
 
         // check if Bluetooth is on:
@@ -48,18 +89,13 @@ var app = {
             notEnabled
         );
     },
-    /*
-        Connects if not connected, and disconnects if connected:
-    */
+    /* Connects if not connected, and disconnects if connected */
     manageConnection: function () {
-
         // connect() will get called only if isConnected() (below)
         // returns failure. In other words, if not connected, then connect:
         var connect = function () {
             // if not connected, do this:
-            // clear the screen and display an attempt to connect
-            app.clear();
-            app.display("Attempting to connect. " +
+            app.notify("Attempting to connect. " +
                 "Make sure the serial port is open on the target device.");
             // attempt to connect:
             bluetoothSerial.connect(
@@ -72,7 +108,8 @@ var app = {
         // disconnect() will get called only if isConnected() (below)
         // returns success  In other words, if  connected, then disconnect:
         var disconnect = function () {
-            app.display("attempting to disconnect");
+            app.clear();
+            app.notify("Attempting to disconnect");
             // if connected, do this:
             bluetoothSerial.disconnect(
                 app.closePort,     // stop listening to the port
@@ -83,21 +120,18 @@ var app = {
         // here's the real action of the manageConnection function:
         bluetoothSerial.isConnected(disconnect, connect);
     },
-    /*
-        subscribes to a Bluetooth serial listener for newline
-        and changes the button:
-    */
+    /* subscribes to a Bluetooth serial listener for newline and changes the button */
     openPort: function () {
         // if you get a good Bluetooth serial connection:
-        app.display("Connected to: " + app.macAddress);
+        app.clear();
+        app.notify("Connected to: " + app.macAddress);
+
         // change the button's name:
         connectButton.innerHTML = "Disconnect";
-        // set up a listener to listen for newlines
-        // and display any new data that's come in since
-        // the last newline:
+        
+        // set up a listener to listen for value terminators
         bluetoothSerial.subscribe(';', function (data) {
-            app.clear();
-            app.display(data);
+            app.update(data);
         });
     },
 
@@ -106,41 +140,41 @@ var app = {
     */
     closePort: function () {
         // if you get a good Bluetooth serial connection:
-        app.display("Disconnected from: " + app.macAddress);
+        app.notify("Disconnected from: " + app.macAddress);
         // change the button's name:
         connectButton.innerHTML = "Connect";
         // unsubscribe from listening:
         bluetoothSerial.unsubscribe(
             function (data) {
-                app.display(data);
+                app.notify(data);
             },
             app.showError
         );
     },
-    /*
-        appends @error to the message div:
-    */
+
+    /* Show error */
     showError: function (error) {
-        app.display(error);
+        app.notify(error);
     },
 
-    /*
-        appends @message to the message div:
-    */
-    display: function (message) {
-        console.log(message);
+    /* Show notifications */
+    notify: function(message) { 
+        var message_div = document.getElementById('messages'),
+            label = document.createTextNode(message),
+            lineBreak = document.createElement("br");     // a line break
 
-        var sensor_div_id = message[0].toLowerCase() + '-value';
+        message_div.appendChild(lineBreak);          // add a line break
+        message_div.appendChild(label);
+    },
+
+    /* Update data from Remote */
+    update: function (raw_data) {
+        console.log(raw_data);
+
+        var sensor_div_id = raw_data[0].toLowerCase() + '-value';
         var sensor_div = document.getElementById(sensor_div_id);
-        if (!sensor_div) {
-            var message_div = document.getElementById('messages'),
-                label = document.createTextNode(message),
-                lineBreak = document.createElement("br");     // a line break
-
-            message_div.appendChild(lineBreak);          // add a line break
-            message_div.appendChild(label);
-        } else {
-            var message_value = message.substr(1, message.length - 2);
+        if (sensor_div) {
+            var message_value = message.substr(1, raw_data.length - 2);
             var display_value = null;
 
             if (sensor_div_id == 't-value') {
@@ -157,9 +191,7 @@ var app = {
             }
         }
     },
-    /*
-        clears the message div:
-    */
+    /* clears the message div: */
     clear: function () {
         var message_div = document.getElementById("messages");
         message_div.innerHTML = "";
